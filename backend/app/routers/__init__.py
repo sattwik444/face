3005184e64421e6ad5ae18959adabdc1bd7efd7d
sattwik444 <mannasattwik444@gmail.com>
2026-02-23 14:23:54 +0530

    Initialize routers in __init__.py

---
+++ backend/app/routers/__init__.py
@@ -0,0 +1,10 @@
+from . import (
+    auth_router,
+    outfit_router,
+    presentation_router,
+    avatar_router,
+    social_router,
+    chat_router,
+    gamification_router,
+    challenge_router
+)
